5ba0a6b43ce796b864ed5870c584c8c8026cd9c5
azusa5526 <azusa5526@gmail.com>
2020-05-28 09:42:27 +0800

    Revert "revert"
    This reverts commit f210c8b92b6929099bd55622abfc74529752e3ff.

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,18 @@
 <script>
 export default {
   name: 'App',
+
+  // created() {
+  //   //api server route + api path(andrew) > 'https://vue-course-api.hexschool.io/api/andrew/products'
+  //   console.log(process.env.API_PATH, process.env.CUSTOM_PATH);
+  //   const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
+  //   this.$http.get(api).then((response) => {
+  //     //console.log(response.data);
+  //   });
+  // }
 }
 </script>
 
 <style lang="scss">
-  @import "./assets/css/all.scss";
+  @import "./assets/all.scss";
 </style>
