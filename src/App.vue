f210c8b92b6929099bd55622abfc74529752e3ff
azusa5526 <azusa5526@gmail.com>
2020-05-28 09:42:14 +0800

    revert

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,9 @@
 <script>
 export default {
   name: 'App',
-
-  // created() {
-  //   //api server route + api path(andrew) > 'https://vue-course-api.hexschool.io/api/andrew/products'
-  //   console.log(process.env.API_PATH, process.env.CUSTOM_PATH);
-  //   const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
-  //   this.$http.get(api).then((response) => {
-  //     //console.log(response.data);
-  //   });
-  // }
 }
 </script>
 
 <style lang="scss">
-  @import "./assets/all.scss";
+  @import "./assets/css/all.scss";
 </style>
