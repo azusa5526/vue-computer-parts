539c0112f78e319a3c2f4564dd5addcd46012766
azusa5526 <azusa5526@gmail.com>
2020-05-28 15:00:42 +0800

    scss調整歸類 將開始撰寫scss

--- src/components/front/FrontNavbar.vue
+++ src/components/front/FrontNavbar.vue
@@ -1,17 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="#">VueComputerParts</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarsExampleDefault"
-      aria-controls="navbarsExampleDefault"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
@@ -71,9 +60,8 @@ export default {
     checkLoginStatus() {
       const api = `${process.env.API_PATH}/api/user/check`;
       const vm = this;
-
+      
       this.$http.post(api).then(response => {
-        //console.log("checkLoginStatus", response.data);
         vm.is_login = response.data.success;
       });
     },
