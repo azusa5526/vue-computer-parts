2c0708eda206ea175026ba9da2e2aefb65c890a6
azusa5526 <azusa5526@gmail.com>
2020-06-26 07:45:27 +0800

    Revert "revert"
    This reverts commit c68b07a786476aafea7b9b3f1994d601f71bd598.

--- src/views/front/FrontProducts.vue
+++ src/views/front/FrontProducts.vue
@@ -4,16 +4,18 @@
     <loading :active.sync="isLoading"></loading>
     <FrontSlidesProducts></FrontSlidesProducts>
 
-    <div class="row mt-4 justify-content-center">
-      <div class="col-xl-2 col-lg-3 col-md-3 col-sm-12 col-12 mb-3">
+    <div class="row mt-4   justify-content-center">
+      <div class="col-xl-2 col-lg-3 col-md-3 col-sm-12 col-12">
         <FrontSidebar
+          ref="frontSidebarComponent"
           :cateFilter="categoryFilter"
           :prodFilter="productsFilter"
           @filterUpdate="updateProductsFilter"
         ></FrontSidebar>
       </div>
 
-      <div id="product-wrap" class="col-xl-10 col-lg-9 col-md-9 col-sm-12 col-12 mb-3">
+      <div id="product-wrap" class="col-xl-10 col-lg-9 col-md-9 col-sm-12 col-12">
+        <!-- BS card -->
         <div class="row">
           <div
             class="col-2xl-3 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-4 product-hover card-wrap"
@@ -31,8 +33,14 @@
                   :style="{backgroundImage: `url(${item.imageUrl})`}"
                 ></div>
                 <div class="card-body">
+                  <!-- <span class="badge badge-secondary float-right ml-2">{{item.category}}</span> -->
                   <h5 class="card-title">{{item.title}}</h5>
                   <p class="card-text">{{item.content}}</p>
+                  <!-- <div class="d-flex justify-content-between align-items-baseline">
+                    <div class="h5" v-if="!item.price">現在只要 {{item.origin_price}} 元</div>
+                    <del class="h6" v-if="item.price">原價 {{item.origin_price}} 元</del>
+                    <div class="h5" v-if="item.price">現在只要 {{item.price}} 元</div>
+                  </div>-->
                 </div>
 
                 <div class="card-footer d-flex justify-content-between align-items-baseline">
@@ -40,6 +48,25 @@
                   <del class="h6" v-if="item.price">{{item.origin_price}} 元</del>
                   <div class="h5" v-if="item.price">現在只要 {{item.price}} 元</div>
                 </div>
+
+                <!-- <div class="card-footer d-flex">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary btn-sm"
+                    @click="getProduct(item.id)"
+                  >
+                    <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+                    查看更多
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-outline-danger btn-sm ml-auto"
+                    @click="addToCart(item.id)"
+                  >
+                    <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+                    加到購物車
+                  </button>
+                </div>-->
               </div>
             </a>
           </div>
@@ -52,6 +79,60 @@
             :pagination="pgnation"
             @changePage="changeCurrentPage"
           ></Pgnation>
+        </div>
+      </div>
+
+      <!-- <div class="no-product-found col-xl-10 col-lg-9 col-md-9 col-sm-12 col-11" v-if="filteredProducts.length == 0">
+        <h4>No products found</h4>
+      </div>-->
+    </div>
+
+    <!-- BS modal -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.imageUrl" class="img-fluid" alt />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">折扣價 {{ product.origin_price }} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+              <div class="h4" v-if="product.price">折扣價 {{ product.price }} 元</div>
+            </div>
+            <select name class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">選購 {{num}} {{product.unit}}</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計
+              <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addToCart(product.id, product.num)"
+            >
+              <i class="fas fa-spinner fa-spin" v-if="status.itemAdding"></i>
+              加到購物車
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -110,6 +191,19 @@ export default {
   },
 
   methods: {
+    // getProducts(page = 1) {
+    //   const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products?page=${page}`;
+    //   const vm = this;
+    //   vm.isLoading = true;
+
+    //   this.$http.get(api).then(response => {
+    //     //console.log(response.data);
+    //     vm.isLoading = false;
+    //     vm.products = response.data.products;
+    //     vm.pagination = response.data.pagination;
+    //   });
+    // },
+
     getAllProducts() {
       const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products/all`;
       const vm = this;
@@ -136,10 +230,7 @@ export default {
             `../front_single_product/${response.data.product.id}`
           );
 
-          sessionStorage.setItem(
-            "rndProds",
-            JSON.stringify(vm.tempRandomProducts)
-          );
+          sessionStorage.setItem("rndProds", JSON.stringify(vm.tempRandomProducts));
           vm.$bus.$emit("getRandomProds", vm.tempRandomProducts);
         }
       });
@@ -314,7 +405,7 @@ export default {
     productsInWindowList() {
       const vm = this;
       let productsInWindow = [];
-      let tempProducts = [];
+      let tempProducts = []; 
 
       vm.filteredProducts = vm.productsFilterList();
       //console.log('filteredProducts',vm.filteredProducts);
@@ -356,3 +447,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
